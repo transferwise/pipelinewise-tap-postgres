3d55cf7c045422b23b019581abf7908b6dde0b32
Peter Kosztolanyi <peter.kosztolanyi@transferwise.com>
2019-07-01 18:54:43 +0100

    bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md') as f:
       long_description = f.read()
 
 setup(name='pipelinewise-tap-postgres',
-      version='1.0.6',
+      version='1.0.7',
       description='Singer.io tap for extracting data from PostgreSQL - PipelineWise compatible',
       long_description=long_description,
       author='Stitch',
