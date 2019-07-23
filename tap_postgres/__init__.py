cbf580f6e0b6e746bb1139268693a750a662d62e
Louis Pieterse <louis.pieterse@transferwise.com>
2019-07-23 12:43:01 +0100

    Simplify logic

--- tap_postgres/__init__.py
+++ tap_postgres/__init__.py
@@ -687,8 +687,8 @@ def main_impl():
                    'dbname'   : args.config['dbname'],
                    'filter_dbs' : args.config.get('filter_dbs'),
                    'filter_schemas' : args.config.get('filter_schemas'),
-                   'debug_lsn' : args.config.get('debug_lsn') == 'true',
-                   'logical_poll_total_seconds': float(args.config.get('logical_poll_total_seconds', 0))}
+                   'debug_lsn' : args.config.get('debug_lsn') == 'true'
+                   }
 
     if args.config.get('ssl') == 'true':
         conn_config['sslmode'] = 'require'
