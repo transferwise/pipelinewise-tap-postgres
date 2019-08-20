8d8f720aff131b65f05b3a604450ae11265722a8
Louis Pieterse <louis.pieterse@transferwise.com>
2019-08-20 17:24:15 +0100

    bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md') as f:
       long_description = f.read()
 
 setup(name='pipelinewise-tap-postgres',
-      version='1.1.2',
+      version='1.1.3',
       description='Singer.io tap for extracting data from PostgreSQL - PipelineWise compatible',
       long_description=long_description,
       long_description_content_type='text/markdown',
@@ -18,7 +18,7 @@ setup(name='pipelinewise-tap-postgres',
       ],
       install_requires=[
           'singer-python==5.3.1',
-          'psycopg2==2.7.4',
+          'psycopg2==2.8.2',
           'strict-rfc3339==0.7',
           'nose==1.3.7'
       ],
