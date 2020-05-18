108bb922ec8de542234c4d752457c48a708a8558
Peter Kosztolanyi <peter.kosztolanyi@transferwise.com>
2020-05-18 15:08:35 +0100

    [AP-700] Fix time zone format

--- tests/test_db.py
+++ tests/test_db.py
@@ -17,8 +17,8 @@ class TestDbFunctions(unittest.TestCase):
 
         # time with time zone values should be converted to UTC and time zone dropped
         # Hour 24 should be taken as 0
-        self.assertEqual(db.selected_value_to_singer_value_impl('12:00:00-08:00', 'time with time zone'), '20:00:00')
-        self.assertEqual(db.selected_value_to_singer_value_impl('24:00:00-08:00', 'time with time zone'), '08:00:00')
+        self.assertEqual(db.selected_value_to_singer_value_impl('12:00:00-0800', 'time with time zone'), '20:00:00')
+        self.assertEqual(db.selected_value_to_singer_value_impl('24:00:00-0800', 'time with time zone'), '08:00:00')
 
         # time without time zone values should be converted to UTC and time zone dropped
         self.assertEqual(db.selected_value_to_singer_value_impl('12:00:00', 'time without time zone'), '12:00:00')
@@ -26,9 +26,9 @@ class TestDbFunctions(unittest.TestCase):
         self.assertEqual(db.selected_value_to_singer_value_impl('24:00:00', 'time without time zone'), '00:00:00')
 
         # timestamp with time zone should be converted to iso format
-        self.assertEqual(db.selected_value_to_singer_value_impl('2020-05-01T12:00:00-08:00',
+        self.assertEqual(db.selected_value_to_singer_value_impl('2020-05-01T12:00:00-0800',
                                                                 'timestamp with time zone'),
-                         '2020-05-01T12:00:00-08:00')
+                         '2020-05-01T12:00:00-0800')
 
         # bit should be True only if elem is '1'
         self.assertEqual(db.selected_value_to_singer_value_impl('1', 'bit'), True)
