ae9b211a9302c3ded42bcbfa06deccefc215dc07
Samira El Aabidi <54845154+Samira-El@users.noreply.github.com>
2021-09-23 15:02:08 +0300

    release 1.8.1 (#126)

--- setup.py
+++ setup.py
@@ -6,11 +6,11 @@ with open('README.md') as f:
     long_description = f.read()
 
 setup(name='pipelinewise-tap-postgres',
-      version='1.8.0',
+      version='1.8.1',
       description='Singer.io tap for extracting data from PostgresSQL - PipelineWise compatible',
       long_description=long_description,
       long_description_content_type='text/markdown',
-      author='TransferWise',
+      author='Wise',
       url='https://github.com/transferwise/pipelinewise-tap-postgres',
       classifiers=[
           'License :: OSI Approved :: GNU Affero General Public License v3',
@@ -33,4 +33,4 @@ setup(name='pipelinewise-tap-postgres',
           tap-postgres=tap_postgres:main
       ''',
       packages=['tap_postgres', 'tap_postgres.sync_strategies']
-)
+      )
