b78ba51f3ff239c8de762f6eaa5406f53ec7b964
Samira El Aabidi <54845154+Samira-El@users.noreply.github.com>
2022-01-05 15:31:31 +0200

    Add docker-compose file and bump pylint (#142)

--- tap_postgres/sync_strategies/full_table.py
+++ tap_postgres/sync_strategies/full_table.py
@@ -44,9 +44,8 @@ def sync_view(conn_info, stream, state, desired_columns, md_map):
         with post_db.open_connection(conn_info) as conn:
             with conn.cursor(cursor_factory=psycopg2.extras.DictCursor, name='stitch_cursor') as cur:
                 cur.itersize = post_db.CURSOR_ITER_SIZE
-                select_sql = 'SELECT {} FROM {}'.format(','.join(escaped_columns),
-                                                        post_db.fully_qualified_table_name(schema_name,
-                                                                                           stream['table_name']))
+                select_sql = f"SELECT {','.join(escaped_columns)} FROM " \
+                             f"{post_db.fully_qualified_table_name(schema_name,stream['table_name'])}"
 
                 LOGGER.info("select %s with itersize %s", select_sql, cur.itersize)
                 cur.execute(select_sql)
@@ -60,7 +59,7 @@ def sync_view(conn_info, stream, state, desired_columns, md_map):
                                                                             time_extracted,
                                                                             md_map)
                     singer.write_message(record_message)
-                    rows_saved = rows_saved + 1
+                    rows_saved += 1
                     if rows_saved % UPDATE_BOOKMARK_PERIOD == 0:
                         singer.write_message(singer.StateMessage(value=copy.deepcopy(state)))
 
@@ -128,17 +127,15 @@ def sync_table(conn_info, stream, state, desired_columns, md_map):
                 xmin = singer.get_bookmark(state, stream['tap_stream_id'], 'xmin')
                 if xmin:
                     LOGGER.info("Resuming Full Table replication %s from xmin %s", nascent_stream_version, xmin)
-                    select_sql = """SELECT {}, xmin::text::bigint
-                                      FROM {} where age(xmin::xid) <= age('{}'::xid)
-                                     ORDER BY xmin::text ASC""".format(','.join(escaped_columns),
-                                                                       fq_table_name,
-                                                                       xmin)
+                    select_sql = f"""
+                        SELECT {','.join(escaped_columns)}, xmin::text::bigint
+                        FROM {fq_table_name} where age(xmin::xid) <= age('{xmin}'::xid)
+                        ORDER BY xmin::text ASC"""
                 else:
                     LOGGER.info("Beginning new Full Table replication %s", nascent_stream_version)
-                    select_sql = """SELECT {}, xmin::text::bigint
-                                      FROM {}
-                                     ORDER BY xmin::text ASC""".format(','.join(escaped_columns),
-                                                                       fq_table_name)
+                    select_sql = f"""SELECT {','.join(escaped_columns)}, xmin::text::bigint
+                                      FROM {fq_table_name}
+                                     ORDER BY xmin::text ASC"""
 
                 LOGGER.info("select %s with itersize %s", select_sql, cur.itersize)
                 cur.execute(select_sql)
@@ -155,7 +152,7 @@ def sync_table(conn_info, stream, state, desired_columns, md_map):
                                                                             md_map)
                     singer.write_message(record_message)
                     state = singer.write_bookmark(state, stream['tap_stream_id'], 'xmin', xmin)
-                    rows_saved = rows_saved + 1
+                    rows_saved += 1
                     if rows_saved % UPDATE_BOOKMARK_PERIOD == 0:
                         singer.write_message(singer.StateMessage(value=copy.deepcopy(state)))
 
