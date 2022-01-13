5b3a8c4eb9f8146bb31166b426803ec9eb9d5ad5
Jonas Kalderstam <jonas@cowboyprogrammer.org>
2022-01-13 16:04:22 +0100

    Fixed keyerror on missing replication key

--- tap_postgres/sync_strategies/incremental.py
+++ tap_postgres/sync_strategies/incremental.py
@@ -118,12 +118,15 @@ def sync_table(conn_info, stream, state, desired_columns, md_map):
 
                     #Picking a replication_key with NULL values will result in it ALWAYS been synced which is not great
                     #event worse would be allowing the NULL value to enter into the state
-                    if record_message.record[replication_key] is not None:
-                        state = singer.write_bookmark(state,
-                                                      stream['tap_stream_id'],
-                                                      'replication_key_value',
-                                                      record_message.record[replication_key])
-
+                    try:
+                        if record_message.record[replication_key] is not None:
+                            state = singer.write_bookmark(state,
+                                                          stream['tap_stream_id'],
+                                                          'replication_key_value',
+                                                          record_message.record[replication_key])
+                    except KeyError as e:
+                        # Replication key not present in table - treat like None
+                        pass
 
                     if rows_saved % UPDATE_BOOKMARK_PERIOD == 0:
                         singer.write_message(singer.StateMessage(value=copy.deepcopy(state)))
