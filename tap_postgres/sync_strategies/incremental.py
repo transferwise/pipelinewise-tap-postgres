923b97282c4e6f70455f97d14d3ea60f3cb3ee28
Samira-El <samira.elaabidi@transferwise.com>
2023-03-29 11:02:38 +0100

    use limit if exists in incremental query

--- tap_postgres/sync_strategies/incremental.py
+++ tap_postgres/sync_strategies/incremental.py
@@ -86,7 +86,9 @@ def sync_table(conn_info, stream, state, desired_columns, md_map):
                                               "replication_key_sql_datatype": replication_key_sql_datatype,
                                               "replication_key_value": replication_key_value,
                                               "schema_name": schema_name,
-                                              "stream": stream})
+                                              "table_name": stream['table_name'],
+                                              "limit": conn_info['limit']
+                                              })
                 LOGGER.info('select statement: %s with itersize %s', select_sql, cur.itersize)
                 cur.execute(select_sql)
 
@@ -111,7 +113,6 @@ def sync_table(conn_info, stream, state, desired_columns, md_map):
                                                       'replication_key_value',
                                                       record_message.record[replication_key])
 
-
                     if rows_saved % UPDATE_BOOKMARK_PERIOD == 0:
                         singer.write_message(singer.StateMessage(value=copy.deepcopy(state)))
 
@@ -122,24 +123,27 @@ def sync_table(conn_info, stream, state, desired_columns, md_map):
 
 def _get_select_sql(params):
     escaped_columns = params['escaped_columns']
-    replication_key = params['replication_key']
+    replication_key = post_db.prepare_columns_sql(params['replication_key'])
     replication_key_sql_datatype = params['replication_key_sql_datatype']
     replication_key_value = params['replication_key_value']
     schema_name = params['schema_name']
-    stream = params['stream']
+    table_name = params['table_name']
+
+    limit_statement = f'LIMIT {params["limit"]}' if params["limit"] else ''
+
     if replication_key_value:
         select_sql = f"""
         SELECT {','.join(escaped_columns)}
         FROM (
             SELECT *
-            FROM {post_db.fully_qualified_table_name(schema_name, stream['table_name'])}
-            WHERE {post_db.prepare_columns_sql(replication_key)} >= '{replication_key_value}'::{replication_key_sql_datatype}
-            ORDER BY {post_db.prepare_columns_sql(replication_key)} ASC
-        ) pg_speedup_trick"""
+            FROM {post_db.fully_qualified_table_name(schema_name, table_name)}
+            WHERE {replication_key} >= '{replication_key_value}'::{replication_key_sql_datatype}
+            ORDER BY {replication_key} ASC {limit_statement}
+        ) pg_speedup_trick;"""
     else:
         # if not replication_key_value
         select_sql = f"""
         SELECT {','.join(escaped_columns)}
-        FROM {post_db.fully_qualified_table_name(schema_name, stream['table_name'])}
+        FROM {post_db.fully_qualified_table_name(schema_name, table_name)}
         """
     return select_sql
